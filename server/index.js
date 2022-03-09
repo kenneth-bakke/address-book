2fa2223968abc37ba7b576fb20ae3aab306f5267
Kenneth Bakke <kennethsbakke@gmail.com>
2022-03-09 10:38:43 -0800

    Added route to serve up build

--- server/index.js
+++ server/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 const router = require('./routes/routes.js');
 const PORT = 8000;
 
 const app = express();
 
+app.use(express.static(path.join(__dirname, '../client/build')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,6 +1,10 @@
 const router = require('express').Router();
+const path = require('path');
 const { contacts } = require('../controllers/');
 
+router.get('', (req, res) => {
+  res.sendFile(path.join(__dirname, '../../client/build/index.html'));
+});
 router.get('/contacts', contacts.getContacts);
 router.post('/contacts', contacts.createContact);
 router.put('/contacts/:contact_id', contacts.editContact);
